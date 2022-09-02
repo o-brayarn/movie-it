c1fad2c0dfcce17b68a27f08fe7b79733b880806
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-09-02 15:54:52 +0300

    update forms to take & record inputs

--- main.js
+++ main.js
@@ -15,26 +15,54 @@ function generateMovieList() {
   fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
-      document.querySelector('#movie-list ul').innerHTML = data.map(movie => `<li onClick="getMovieDetails(${movie.id})">${movie.title}</li>`).join('');
+      document.querySelector('#movie-list ul').innerHTML = data.map(movie => `<li onClick="getMovieDetails(${movie.id})">${movie.title}</li>`)
+        .join('');
     })
 }
+
+
+var reviewForm = document.getElementById("review-form")
+reviewForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  var comment = document.querySelector("#review-input").value;
+  document.querySelector('#review-list ul').innerHTML += `<li>${comment}</li>`;
+  reviewForm.reset()
+})
+
+
+function suggestMovie() {
+  let form = document.querySelector('#suggest-form')
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    let movieTitle = document.getElementById('movie-name').value
+    let poster = document.getElementById('movie-src').value
+    let descript = document.getElementById('movie-des').value
+    let movieReview = document.getElementById('movie-rev').value
+
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        title: movieTitle,
+        image: poster,
+        description: descript,
+        reviews: movieReview
+      })
+    })
+      .then(res => res.json())
+      .then(data => data)
+      .catch(error => error)
+  })
+}
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
   getMovieDetails(1)
   generateMovieList();
-
-  var reviewForm = document.getElementById("review-form")
-  reviewForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    var comment = document.querySelector("#review-input").value;
-    document.querySelector('#review-list ul').innerHTML += `<li>${comment}</li>`;
-    reviewForm.reset()
-  })
-
-
-  var suggestionForm = document.querySelector('#suggest-form');
-  suggestionForm.addEventListener('submit', e => {
-    e.preventDefault();
-  })
+  suggestMovie()
 })
